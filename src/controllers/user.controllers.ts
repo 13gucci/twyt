39bc087ffdab35a2017b4015a88b402bc9946705
13gucci <bibabibum0110@gmail.com>
2024-06-30 15:20:26 +0700

    💭 test api register

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,4 +1,7 @@
 import { Request, Response } from 'express';
+import User, { IUser } from '~/models/schemas/user.schema';
+import databaseService from '~/services/database.services';
+import userServices from '~/services/user.services';
 
 export const loginController = (req: Request, res: Response) => {
     const { email, password } = req.body;
@@ -12,4 +15,20 @@ export const loginController = (req: Request, res: Response) => {
     return res.status(400).json({
         message: 'Username or password failed'
     });
+};
+
+export const registerController = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    try {
+        const response = await userServices.register({ email, password });
+
+        return res.json({
+            message: 'register success',
+            response
+        });
+    } catch (err) {
+        return res.json({
+            message: 'register failed'
+        });
+    }
 };
