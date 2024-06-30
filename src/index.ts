e2a8b4c70aa4c94810e01a651758872c5449bb3d
13gucci <bibabibum0110@gmail.com>
2024-06-30 12:46:17 +0700

    💭 finished setup database

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import userRouters from './routes/user.routes';
+import database from './services/database.services';
 
 const app = express();
 const port = 3000;
@@ -7,6 +8,7 @@ const port = 3000;
 app.use(express.json());
 
 app.use('/api', userRouters);
+database.connect();
 
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
